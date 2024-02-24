5e808b93f4894075304e200e77a44593ae4de54a
Sanya_lsdadsa <Semen@yand.sadas>
2024-02-24 17:08:35 +0300

    v1.3

---
+++ src/main/java/MainSQLite.java
@@ -0,0 +1,14 @@
+import java.sql.*;
+
+public class MainSQLite {
+    public static void main(String[] args) throws SQLException {
+        Connection connection = DriverManager.getConnection("jdbc:sqlite:src/resources/db/db.sqlite");
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("SELECT * FROM leha;");
+        while (resultSet.next()){
+            int id = resultSet.getInt(1);
+            String da = resultSet.getString(2);
+            System.out.println(id + " "+ da);
+        }
+    }
+}
